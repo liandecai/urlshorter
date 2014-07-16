84320c1725f0b186b30eb0eb225f85201d01fe7e
liandecai <cailiande0@163.com>
2014-07-16 11:26:29 -0400

    delete useless

--- src/main/java/liande/cai/urlshorter/dao/URLMappingDao.java
+++ src/main/java/liande/cai/urlshorter/dao/URLMappingDao.java
@@ -6,16 +6,17 @@ import liande.cai.urlshorter.model.URLInfo;
 
 import org.springframework.stereotype.Repository;
 
-/**   
+/**
  * Description:
+ * 
  * @author liandecai
  * @time Jun 21, 2014
  */
 @Repository
 public interface URLMappingDao {
-    
+
     public int insert(URLInfo urlInfo);
-    
+
     public URLInfo query(Map<String, Object> map);
-    
+
 }
